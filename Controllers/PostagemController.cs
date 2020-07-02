663b4e689e452dae6bbbfadb249474f02a19b3c6
Andrade <gabriellolwin@gmail.com>
2020-07-01 22:39:35 -0300

    ALOUS

---
+++ Controllers/PostagemController.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using RestSharp;
+
+namespace Projeto_Framework.Controllers
+{
+    public class PostagemController : Controller
+    {
+        public IActionResult Index()
+        {
+            string sTabela = "";
+
+            var client = new RestClient("https://jsonplaceholder.typicode.com/");
+
+            var request = new RestRequest("posts");
+
+            var response = client.Get(request);
+
+            JArray resposta = (JArray)JsonConvert.DeserializeObject(response.Content);
+
+            foreach(JObject trabalho in resposta) {
+                sTabela += "<tr>";
+                sTabela += "<td>";
+                sTabela += trabalho["userId"];
+                sTabela += "</td>";
+                sTabela += "<td>";
+                sTabela += trabalho["id"];
+                sTabela += "</td>";
+                sTabela += "<td>";
+                sTabela += trabalho["title"];
+                sTabela += "</td>";
+                sTabela += "<td>";
+                sTabela += trabalho["body"];
+                sTabela += "</td>";
+                sTabela += "</tr>";
+
+
+            }
+
+            ViewBag.projeto = sTabela;
+
+            return View();
+        }
+    }
+}
---
+++ Controllers/TodosController.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using RestSharp;
+
+namespace Projeto_Framework.Controllers
+{
+    public class TodosController : Controller
+    {
+        public IActionResult Index()
+        {
+            string sTabela = "";
+
+            var client = new RestClient("https://jsonplaceholder.typicode.com/");
+
+            var request = new RestRequest("todos");
+
+            var response = client.Get(request);
+
+            JArray resposta = (JArray)JsonConvert.DeserializeObject(response.Content);
+
+            foreach (JObject trabalho in resposta)
+            {
+                sTabela += "<tr>";
+                sTabela += "<td>";
+                sTabela += trabalho["userId"];
+                sTabela += "</td>";
+                sTabela += "<td>";
+                sTabela += trabalho["id"];
+                sTabela += "</td>";
+                sTabela += "<td>";
+                sTabela += trabalho["title"];
+                sTabela += "</td>";
+                sTabela += "<td>";
+                sTabela += trabalho["completed"];
+                sTabela += "</td>";
+                sTabela += "</tr>";
+
+
+            }
+
+            ViewBag.projeto = sTabela;
+
+            return View();
+        }
+    }
+}
